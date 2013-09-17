cfa93ca2132f8a09971b4f6f5c062507f0060a69
Felix Ableitner <me@nutomic.com>
2013-09-17 23:18:12 +0200

    Added config file for resolution and window style.

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,22 @@
  */
 int main(int argc, char* argv[]) {
 	Yaml::setFolder("res/yaml/");
-
 	Loader::i().setFolder("res/");
 	Loader::i().setSubFolder<sf::Texture>("textures/");
 
-    tgui::Window window(sf::VideoMode(1024, 768, 32), "Dungeon Gunner",
-				sf::Style::Close | sf::Style::Titlebar);
+	Yaml windowConfig("window.yaml");
+	sf::VideoMode mode(windowConfig.get("resolution_width", 800),
+	                   windowConfig.get("resolution_height", 600),
+	                   32);
+	sf::Uint32 style;
+	if (windowConfig.get("fullscreen", false))
+		style = sf::Style::Fullscreen;
+	else if (windowConfig.get("borderless", false))
+		style = sf::Style::None;
+	else
+		style = sf::Style::Titlebar;
+    tgui::Window window(mode, "Dungeon Gunner", style);
+
     Vector2f::SCREEN_HEIGHT = window.getSize().y;
 
 	if (!window.globalFont.loadFromFile("res/DejaVuSans.ttf"))
